49647fab7de66f5f4a3d70b7cd9617162bbabb49
Maxime Daymard <mdaymard@gmail.com>
2025-02-25 13:28:41 +0100

    Add App Constants

---
+++ Stories/Sources/Common/AppConstants.swift
@@ -0,0 +1,12 @@
+//
+//  AppConstants.swift
+//  Stories
+//
+//  Created by Maxime Daymard on 25/02/2025.
+//
+
+import Foundation
+
+enum AppConstants {
+    static let cacheDirectory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+}
