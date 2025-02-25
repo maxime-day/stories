d9b0f4ccd2775c19bd5abacefbe07ecb62bdb46f
Maxime Daymard <mdaymard@gmail.com>
2025-02-25 10:30:09 +0100

    Initial commit

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by Maxime Daymard on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoriesTests/StoriesTests.swift
@@ -0,0 +1,16 @@
+//
+//  StoriesTests.swift
+//  StoriesTests
+//
+//  Created by Maxime Daymard on 25/02/2025.
+//
+
+import Testing
+
+struct StoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
