74df7dee2401553b589fd89941b80e5ebd561203
Maxime Daymard <mdaymard@gmail.com>
2025-02-25 13:28:34 +0100

    Fix StoriesApp

--- Stories/Sources/StoriesApp.swift
+++ Stories/Sources/StoriesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StoriesApp: App {
     var body: some Scene {
         WindowGroup {
-            StoriesView()
+            StoriesView(viewModel: StoriesViewModel())
         }
     }
 }
